5c83e15ebd2525fba25de66435741097c6a6afc1
Enzo Ramirez <enzoramirez11@gmail.com>
2021-10-04 02:33:33 -0300

    cambio en javascript

--- js/codigo.js
+++ js/codigo.js
@@ -3,3 +3,14 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
+let days= document.getElementsByClassName("day-box")
+let today = new Date()
+for (var i=0; i<days.length;i++) {
+  days[0-i]=days[7-i]
+  if (today.getDay()==i) {
+    days[i-2].classList.add("bg-warning");
+    days[i-1].classList.remove("bg-secondary")
+    days[i-1].classList.add("bg-primary");
+    days[i].classList.add("bg-warning");
+  }
+}
